6a1c09b788fcf06633aa0f6b54f5f03e2683b10c
Matthew Mickler <matthewmickler@Matthews-MBP.hitronhub.home>
2022-06-24 08:23:05 -0700

    randomcolor commit

--- script.js
+++ script.js
@@ -1,48 +1,3 @@
-// var container = document.createElement('div');
-// container.style.width = "100%";
-// document.body.append(container);
-
-// for(var i = 0; i < 8; i++){
-//     for(var j = 0; j < 8; j++){
-
-//         var div = document.createElement('div');
-//         if(i % 2 !== 0){
-//             if(j % 2 !== 0){
-//                 div.style.width = '11.1%';
-//                 div.style.float = 'left';
-//                 div.style.paddingBottom = '11.1%'
-//                 div.style.background = 'red';
-//                 div.style.border = '1px solid black';
-//                 container.append(div);
-//             } else {
-//                 div.style.width = '11.1%';
-//                 div.style.float = 'left';
-//                 div.style.paddingBottom = '11.1%'
-//                 div.style.background = 'black';
-//                 div.style.border = '1px solid black';
-//                 container.append(div);
-//             }
-//         } else {
-//             if(j === 0 || j % 2 === 0){
-//                 div.style.width = '11.1%';
-//                 div.style.float = 'left';
-//                 div.style.paddingBottom = '11.1%'
-//                 div.style.background = 'red';
-//                 div.style.border = '1px solid black';
-//                 container.append(div);
-//             } else {
-//                 div.style.width = '11.1%';
-//                 div.style.float = 'left';
-//                 div.style.paddingBottom = '11.1%'
-//                 div.style.background = 'black';
-//                 div.style.border = '1px solid black';
-//                 container.append(div);
-//             }
-//         }
-//     }    
-// }
-
-
 var container = document.createElement("div");
 container.style.width = "100%";
 document.body.append(container);
@@ -52,15 +7,15 @@ for (var i = 0; i < 8; i++) {
     var div = document.createElement("div");
     if (i % 2 !== 0) {
       if (j % 2 !== 0) {
-        div.style.background = "red";
+        div.style.background = '#' + Math.floor(Math.random()*16777215).toString(16);
       } else {
-        div.style.background = "black";
+        div.style.background = '#' + Math.floor(Math.random()*16777215).toString(16);
       }
     } else {
       if (j === 0 || j % 2 === 0) {
-        div.style.background = "red";
+        div.style.background = '#' + Math.floor(Math.random()*16777215).toString(16);
       } else {
-        div.style.background = "black";
+        div.style.background = '#' + Math.floor(Math.random()*16777215).toString(16);
       }
     }
     div.style.width = "11.1%";
